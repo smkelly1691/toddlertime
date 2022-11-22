ce8e99f3b979a3f95167be1cc7c0c6ec3ea9c1fc
smkelly1691 <skelly1691@gmail.com>
2022-11-22 17:53:43 -0500

    creates Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+    return (
+        <div className="Logo">
+            <div>TODDLER TIME</div>
+            <div>Every Day is an Adventure</div>
+        </div>
+    );
+}
