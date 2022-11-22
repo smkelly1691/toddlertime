49021fbba2f6d85f30a9cb1fbd4eff4429c128f6
smkelly1691 <skelly1691@gmail.com>
2022-11-21 21:41:18 -0500

    mounts activities router with protection in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const ensureLoggedIn = require('./config/ensureLoggedIn')
 
 require('dotenv').config()
 require('./config/database')
@@ -16,7 +17,10 @@ app.use(express.static(path.join(__dirname, 'build')))
 
 app.use(require('./config/checkToken'))
 
-app.use('/api/users', require('./routes/api/users'))
+app.use('/api/users', require('./routes/api/users'));
+
+const ensureLoggedIn = require('./config/ensureLoggedIn');
+app.use('/api/activities', ensureLoggedIn, require('./routes/api/activities'));
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
