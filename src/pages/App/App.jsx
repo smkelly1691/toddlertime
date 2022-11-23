9bbfabb4d03668a7af0c350b1dc02625571b2b06
smkelly1691 <skelly1691@gmail.com>
2022-11-23 00:58:19 -0500

    styling

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { Navigate } from 'react-router-dom';
 import './App.css';
 import { getUser } from '../../utilities/users-service'
 import AuthPage from '../AuthPage/AuthPage'
 import NewPlanPage from '../NewPlanPage/NewPlanPage'
 import PlanHistoryPage from '../PlanHistoryPage/PlanHistoryPage'
-import NavBar from '../../components/NavBar/NavBar'
 
 export default function App() {
   const [user, setUser] = useState(getUser())
@@ -13,9 +13,9 @@ export default function App() {
   return (
     <main className="App">
       { user ? 
-        <>
-          <NewPlanPage user={user} setUser={setUser} />
-        </>
+      <>
+        <NewPlanPage user={user} setUser={setUser} />
+      </>
         :
         <AuthPage setUser={setUser}/>
       }
